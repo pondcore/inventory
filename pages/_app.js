926c4caf88e3b48696badea9f70c87adbdf12c28
pondcore <anucha.pinsaimoon@gmail.com>
2021-08-07 16:11:43 +0700

    new branch Layout

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,10 @@ function MyApp({ Component, pageProps }) {
       <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">รายงาน</Menu.Item>
+          <Menu.Item key="2">จัดการสินค้า</Menu.Item>
+          <Menu.Item key="3">จัดการลูกค้า</Menu.Item>
+          <Menu.Item key="3">จัดการออเดอร์</Menu.Item>
         </Menu>
       </Header>
       <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
